94ca45900a24e89d485198e2a24bc900153f8eb4
Bruno Breda Rossi <brunorossi.1@hotmail.com>
2021-10-25 12:49:27 -0300

    ver1

---
+++ src/app/faturas.ts
@@ -0,0 +1,37 @@
+export const faturas = [
+  {
+    cod: 1,
+    empresa: "Bruno Breda Rossi",
+    valor: 3414784,
+    dt_vencto: "Análise e Desenvolvimento de Sistemas",
+    dt_pgto: "19/09/1996"
+  },
+  {
+    cod: 2,
+    empresa: "Nome de teste um",
+    valor: 1234567,
+    dt_vencto: "Economia",
+    dt_pgto: "01/01/2000"
+  },
+  {
+    cod: 3,
+    empresa: "Nome de teste dois",
+    valor: 7654321,
+    dt_vencto: "Pesca",
+    dt_pgto: "11/11/2001"
+  },
+  {
+    cod: 4,
+    empresa: "Nome de teste tres",
+    valor: 9876543,
+    dt_vencto: "Corte e costura",
+    dt_pgto: "22/05/1992"
+  },
+  {
+    cod: 5,
+    empresa: "Nome de teste quatro",
+    valor: 6541231,
+    dt_vencto: "Engenharia de produção",
+    dt_pgto: "01/01/2002"
+  }
+];
